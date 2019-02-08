44ea96f6a7ad0fd408a26c579f0399642b54780a
Britney <bp018@email.uark.edu>
2019-02-08 00:35:24 -0600

    Tracker 2. Changes to be more inclusive

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,5 +1,6 @@
 class HelloWorldApp{
 	public static void main(String[] args){
-		System.out.println("Hello World!");// display the string
+		System.out.println("Hello Universe!");// display the string
+		System.out.println("From Earth.");//Tracker 2. be more inclusive
 	}
 }
